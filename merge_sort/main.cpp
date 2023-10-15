60c720e0060bcd020523b887dcce97a13ab73202
bukkalexander <alexander.bukk@gmail.com>
2023-10-15 20:01:40 +0200

    refactor (merge_sort): simplify merge
    Create two subarray and fill the original array with the sorted
    values. This solution feels simpler than copying the original
    array into a new one, and then fill it with the sorted values,
    and copy them over to the original array. The simplification has
    to do with the loop end conditions which become simpler when they
    are stated in terms of the length of the subarray, rather than a
    last value in the original array.

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -2,18 +2,12 @@
 
 #include <iostream>
 
-void sort(int* a, int i_start, int i_end);
+void merge_sort(int* a, int i_start, int i_end);
 
 void merge(int* a, int i_start, int i_mid, int i_end);
 
-void merge_sort(int* a, int n) {
-    
-    sort(a, 0, n-1);
-}
-
-void sort(int* a, int i_start, int i_end) {
+void merge_sort(int* a, int i_start, int i_end) {
     // base case
-
     std::cout << "sorting " << i_start << " " << i_end << std::endl;
     if (i_start == i_end) {
         return;
@@ -22,12 +16,13 @@ void sort(int* a, int i_start, int i_end) {
     // divide
     // calculate the middle element index i_mid
     // i_start + (i_end - i_start) / 2 = (2*i_start + i_end - i_start) / 2 = (i_start + i_end) / 2
-    int i_mid = (i_start + i_end) / 2;
+    int i_mid = (i_start + i_end) / 2; // floor
     std::cout << "dividing " << i_mid << std::endl;
+
     // conquer
     // recursively sort each sub array a[i_start:i_mid], a[i_mid+1:i_end]
-    sort(a, i_start, i_mid);
-    sort(a, i_mid + 1, i_end);
+    merge_sort(a, i_start, i_mid);
+    merge_sort(a, i_mid + 1, i_end);
     
 
     // combine (merge)
@@ -38,58 +33,55 @@ void sort(int* a, int i_start, int i_end) {
 void merge(int* a, int i_start, int i_mid, int i_end) {
     // merge the already sorted sub arrays a[i_start:i_mid] and a[i_mid+1:i_end]
     // into a single sorted array a[i_start:i_end]
-
-    int n = i_end - i_start + 1;
-    int i_b = 0;
-    int *b = new int[n];
-
-    int i_left_start = i_start;
-    int i_left_end = i_mid;
-    int i_left_out = i_left_end + 1;
-
-    int i_right_start = i_mid + 1;
-    int i_right_end = i_end;
-    int i_right_out = i_right_end + 1;
     
-    int i_left = i_left_start;
-    int i_right = i_right_start;
+    int n_left = i_mid - i_start + 1;
+    int n_right = i_end - i_mid;
 
-    while (i_b < n && i_left < i_left_out && i_right < i_right_out) {
-        if (a[i_left] < a[i_right]) {
-            b[i_b] = a[i_left];
+    int *left = new int[n_left];
+    int *right = new int[n_right];
+
+    for (int i = 0; i < n_left; i++) {
+        left[i] = a[i_start + i];
+    }
+
+    for (int i = 0; i < n_right; i++) {
+        right[i] = a[i_mid + 1 + i];
+    }
+
+    int i_left = 0;
+    int i_right = 0;
+    int i_a = i_start;
+    while (i_left < n_left && i_right < n_right) {
+        if (left[i_left] < right[i_right]) {
+            a[i_a] = left[i_left];
             i_left++;
         } else {
-            b[i_b] = a[i_right];
+            a[i_a] = right[i_right];
             i_right++;
         }
-        i_b++;
+        i_a++;
     }
 
-    while (i_left < i_left_out) {
-        b[i_b] = a[i_left];
+    while (i_left < n_left) {
+        a[i_a] = left[i_left];
         i_left++;
-        i_b++;
+        i_a++;
     }
 
-    while (i_right < i_right_out) {
-        b[i_b] = a[i_right];
+    while (i_right < n_right) {
+        a[i_a] = right[i_right];
         i_right++;
-        i_b++;
+        i_a++;
     }
-
-    // copy the sorted array b into a[i_start:i_end]
-    for (int i = 0; i < n; i++) {
-        a[i_start+i] = b[i];
-    }
-
-    delete[] b;
+    
+    delete[] left;
+    delete[] right;
 
 }
 
 int main() {
     int n = 6;
-    int a[] = {5, 2, 7, 1, 3, -6};
-
+    int a[] = {5, 0, 5, -1, 3, -6};
 
     std::cout << "================================================" << std::endl;
     
@@ -97,7 +89,7 @@ int main() {
     print_array(a, n);
 
     std::cout << "Sorting..." << std::endl;
-    merge_sort(a, n);
+    merge_sort(a, 0, n-1);
     std::cout << "a = ";
     print_array(a, n);
 
@@ -109,7 +101,6 @@ int main() {
         std::cout << "false";
     }
     std::cout << std::endl;
-    std::cout << "is_a_sorted=" << is_a_sorted << std::endl;
     std::cout << "================================================" << std::endl;
     return 0;
 }
